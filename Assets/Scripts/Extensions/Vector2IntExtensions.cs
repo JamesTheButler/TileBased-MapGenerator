215f1a76a2f4d7af19f35a553d51ad4cd674a373
Tom <tom.ille@gmx.de>
2022-02-04 09:51:57 +0100

    add culling of 2x2 road sections

--- Assets/Scripts/Extensions/Vector2IntExtensions.cs
+++ Assets/Scripts/Extensions/Vector2IntExtensions.cs
@@ -13,4 +13,9 @@ public static class Vector2IntExtensions {
     public static Vector2Int ToVector2Int(this Point2D point) {
         return new Vector2Int(point.X, point.Y);
     }
+
+    /* Returns true if x and y of this vector are > 0 and < other*/
+    public static bool IsInside(this Vector2Int thisVector, Vector2Int other) {
+        return thisVector.x > 0 && thisVector.y > 0 && thisVector.x < other.x && thisVector.y < other.y;
+    }
 }
--- Assets/Scripts/Map Generation/TileGenerators/PathingTileGenerator.cs
+++ Assets/Scripts/Map Generation/TileGenerators/PathingTileGenerator.cs
@@ -4,18 +4,17 @@ using Pathfinding.AStar;
 using Pathfinding.General;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class PathingTileGenerator : BaseTileGenerator {
     public TileType nodeType;
     public float neighborRadius;
+    public bool enabled2x2Culling = false;
 
     public event MapGenerator.NeighborsGeneratedEvent OnNeighborsGenerated;
 
-    //TODO: get from somewhere?
+    //TODO: get from somewhere? 
     public float PATHCOST = 0.5f;
-
     public HeuristicType heuristicType;
 
     public override bool[,] GenerateTiles(TileTypeMap tileTypeMap) {
@@ -23,39 +22,38 @@ public class PathingTileGenerator : BaseTileGenerator {
         if (!IsEnabled) return thisLayer;
         if (!tileTypeMap.HasLayer(nodeType)) return thisLayer;
 
+        // set up paths between nodes
         var nodeTiles = tileTypeMap.GetTiles(nodeType);
-
-        var neighborDict = new Dictionary<Vector2Int, List<Vector2Int>>();
-
-        foreach (var node in nodeTiles) {
-            var neighbors = new List<Vector2Int>();
-
-            foreach (var potentialNeighbor in nodeTiles) {
-                if (node != potentialNeighbor && Vector2Int.Distance(node, potentialNeighbor) < neighborRadius) {
-                    neighbors.Add(potentialNeighbor);
-                }
-
-            }
-            if (neighbors.IsEmpty().Not()) {
-                neighborDict.Add(node, neighbors);
-            }
-        }
-        OnNeighborsGenerated?.Invoke(neighborDict);
-
-        var gridMap = new GridTree2D(tileTypeMap.ToCostField());
-        var pathNodes = new List<Vector2Int>();
-
+        var neighborDict = CreateNeighbors(nodeTiles);
         var paths = ListDictToTupleList(neighborDict);
-        Debug.Log(paths.Aggregate("Paths BEFORE: ", (arg1, arg2) => arg1 + $"<{arg2.Item1}|{arg2.Item2}>"));
         paths.Shuffle(seed);
-        Debug.Log(paths.Aggregate("Paths AFTER: ", (arg1, arg2) => arg1 + $"<{arg2.Item1}|{arg2.Item2}>"));
+
+        // search
         Heuristic heuristic;
-        if(heuristicType == HeuristicType.MANHATTAN) {
+        if (heuristicType == HeuristicType.MANHATTAN) {
             heuristic = new ManhattanHeuristic();
-        }else {
+        } else {
             heuristic = new StraightLineHeuristic();
         }
-        
+
+        // generate path tiles via AStarSearch
+        var pathTiles = GeneratePathTiles(tileTypeMap, paths, heuristic);
+
+        // update layer
+        foreach (var pathTile in pathTiles) {
+            thisLayer[pathTile.x, pathTile.y] = true;
+        }
+
+        // cull 2x2 path blocks
+        if (enabled2x2Culling) {
+            Cull2x2Crossings(thisLayer, nodeTiles, tileTypeMap.size);
+        }
+        return thisLayer;
+    }
+
+    private List<Vector2Int> GeneratePathTiles(TileTypeMap tileTypeMap, List<Tuple<Vector2Int, Vector2Int>> paths, Heuristic heuristic) {
+        var pathTiles = new List<Vector2Int>();
+        var gridMap = new GridTree2D(tileTypeMap.ToCostField());
         int i = 0;
         foreach (var entry in paths) {
             i++;
@@ -71,14 +69,93 @@ public class PathingTileGenerator : BaseTileGenerator {
                 intField[node.x, node.y] = PATHCOST;
             }
             gridMap.Update(intField);
-            pathNodes.AddRange(path);
+            pathTiles.AddRange(path);
+        }
+        return pathTiles;
+    }
+
+    private Dictionary<Vector2Int, List<Vector2Int>> CreateNeighbors(List<Vector2Int> nodeTiles) {
+        var neighborDict = new Dictionary<Vector2Int, List<Vector2Int>>();
+        foreach (var node in nodeTiles) {
+            var neighbors = new List<Vector2Int>();
+
+            foreach (var potentialNeighbor in nodeTiles) {
+                if (node != potentialNeighbor && Vector2Int.Distance(node, potentialNeighbor) < neighborRadius) {
+                    neighbors.Add(potentialNeighbor);
+                }
+            }
+
+            if (neighbors.IsEmpty().Not()) {
+                neighborDict.Add(node, neighbors);
+            }
         }
 
-        foreach (var node in pathNodes) {
-            thisLayer[node.x, node.y] = true;
+        OnNeighborsGenerated?.Invoke(neighborDict);
+        return neighborDict;
+    }
+
+    /* Goes through tile layer and finds all 2x2 patches. */
+    private List<Vector2Int> FindTopRightCornersOf2x2Patches(bool[,] pathTileLayer, Vector2Int mapSize) {
+        var corners = new List<Vector2Int>();
+        // 'mapSize - 1' to not go into corner and get out of bounds with 'pathTileLayer[x + 1, y + 1]'
+        for (int x = 0; x < mapSize.x - 1; x++) {
+            for (int y = 0; y < mapSize.y - 1; y++) {
+                // if all in a 2x2 block are true
+                if (pathTileLayer[x, y] && pathTileLayer[x + 1, y] && pathTileLayer[x, y + 1] && pathTileLayer[x + 1, y + 1]) {
+                    corners.Add(new Vector2Int(x, y));
+                }
+            }
+        }
+        return corners;
+    }
+
+    private void Cull2x2Crossings(bool[,] pathTileLayer, List<Vector2Int> nodeTileList, Vector2Int mapSize) {
+        var topRightCornersOf2x2Patches = FindTopRightCornersOf2x2Patches(pathTileLayer, mapSize);
+
+        var deltas = new List<Vector2Int> {
+            Vector2Int.up,
+            Vector2Int.right,
+            Vector2Int.down,
+            Vector2Int.left,
+        };
+
+        var cullList = new List<Vector2Int>();
+
+        // foreach 2x2 patch
+        foreach (var topTile in topRightCornersOf2x2Patches) {
+            var patchTiles = new List<Vector2Int>{
+                topTile,
+                new Vector2Int(topTile.x + 1, topTile.y),
+                new Vector2Int(topTile.x, topTile.y + 1),
+                new Vector2Int(topTile.x + 1, topTile.y + 1),
+            };
+
+            foreach (var patchTile in patchTiles) {
+                var count = 0;
+                var hasNode = nodeTileList.Contains(patchTile);
+                for (int i = 0; i < 4; i++) {
+                    var adjacentTilePos = new Vector2Int(patchTile.x + deltas[i].x, patchTile.y + deltas[i].y);
+                    if (adjacentTilePos.IsInside(mapSize)) {
+                        if (nodeTileList.Contains(adjacentTilePos)) {
+                            hasNode = true;
+                            break;
+                        }
+                        if (pathTileLayer[adjacentTilePos.x, adjacentTilePos.y]) {
+                            count++;
+                        }
+                    }
+                }
+
+                if (!hasNode && count == 2) {   // for tiles with 2 neighboring tiles and no adjacent node: add to cull list
+                    cullList.Add(patchTile);
+                }
+            }
         }
 
-        return thisLayer;
+        // cull tiles
+        foreach (var tile in cullList) {
+            pathTileLayer[tile.x, tile.y] = false;
+        }
     }
 
     private List<Tuple<Vector2Int, Vector2Int>> ListDictToTupleList(Dictionary<Vector2Int, List<Vector2Int>> dict) {
